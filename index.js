bfc2848aa5fefed94d91ea8063e2dbd5301158d2
Prashanith <prashanith007@gmail.com>
2024-10-07 17:39:13 +0530

    added_apis

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-import { createBrands } from "./brands.js";
 import { config } from "dotenv";
-import { createManufacturers } from "./manufacturers.js";
+import { createBrands } from "./test/brands.js";
+import { createManufacturers } from "./test/manufacturers.js";
+import { createWarehouses } from "./test/warehouses.js";
+import { createAccounts } from "./test/accounts.js";
 
 config();
 createBrands();
 createManufacturers();
+createWarehouses();
+createAccounts();
