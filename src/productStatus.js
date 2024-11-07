ccd710604e72aee6ee1b4b0a074d475621959d09
Prashanith <prashanith007@gmail.com>
2024-11-08 00:22:10 +0530

    fix

--- src/productStatus.js
+++ src/productStatus.js
@@ -6,7 +6,7 @@ async function updateProductStatus() {
   const api = "https://qa-admin-api.ecoyaan.com/v1/Product";
 
   for (let i = 2; i < 66; i++) {
-    const res = await axios.post(
+    const res = await axios.patch(
       api,
       {
         product_fk: i,
