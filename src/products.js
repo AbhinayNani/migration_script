2e453fa362ee4fe5c5bc27268ba19dd13e8e3bf0
Prashanith <prashanith007@gmail.com>
2024-11-07 21:58:10 +0530

    migrate

--- src/products.js
+++ src/products.js
@@ -1,18 +1,10 @@
 import axios from "axios";
 import https from "https";
 import adidas from "./adidas.json" assert { type: "json" };
-import nike from "./nikeClothes.json" assert { type: "json" };
+import nike from "./try.json" assert { type: "json" };
 
 async function createProducts() {
-  const api = `https://localhost:5174/v1/Product`;
-
-  for (const product of adidas) {
-    const res = await axios.post(api, product, {
-      httpsAgent: new https.Agent({
-        rejectUnauthorized: false,
-      }),
-    });
-  }
+  const api = `https://qa-admin-api.ecoyaan.com/v1/Product`;
 
   for (const product of nike) {
     const res = await axios.post(api, product, {
@@ -22,5 +14,7 @@ async function createProducts() {
     });
   }
 }
+
+createProducts();
 
 export { createProducts };
